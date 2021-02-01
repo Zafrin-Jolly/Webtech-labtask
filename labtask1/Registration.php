36617e66c927b080f80f63705245202a93727299
Zafrin-Jolly <72722584+Zafrin-Jolly@users.noreply.github.com>
2021-02-01 13:33:49 +0600

    Registration form

---
+++ labtask1/Registration.php
@@ -0,0 +1,35 @@
+<DOCTYPE HTML>
+<html>
+<head><b>Welcome to my page</b></head>
+<title>Lab-task-1</title>
+<body>
+<h1>REGISTRATION</h1>
+
+<form>
+<table>
+<tr>
+<td>Name:</td>
+<input type="text"id="f"name="fname">
+</tr>
+<tr>
+<td>Email:</td>
+<input type="text"id="e"name="ename">
+</tr>
+<tr>
+<td>User name:</td>
+<input type="text"id="u"name="uname">
+</tr>
+<tr>
+<td>password:</td>
+<input type="text"id="p"name="pname">
+</tr>
+<tr>
+<td>confirm password:</td>
+<input type="text"id="p"name="pname">
+</tr>
+<input type="submit" Value="submit">
+<input type="submit" Value="submit">
+
+</form>
+</body>
+</html>
